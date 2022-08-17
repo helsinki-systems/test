7ed5c42411c7f065f7ec25b339080c4cbbd8df6c
ajs124 <git@ajs124.de>
2022-08-17 14:31:27 +0200

    use nixpkgs from nix path

--- release.nix
+++ release.nix
@@ -1,7 +1,4 @@
 let
-  pkgs = builtins.fetchGit {
-    url = "https://github.com/nixos/nixpkgs";
-    rev = "560f53d726430cd567bf45f98e649ecad42776a5";
-  };
+  pkgs = import <nixpkgs> {};
 in
   pkgs.hello
